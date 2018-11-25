3cf221754e7e244cd14173258031bbe4e30f29a7
kadosh1000 <kadosh10001@gmail.com>
2018-11-25 13:26:34 +0200

    * fix plugin structure

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const child_process = require("child_process");
 
-
 const DEPLOY_AN_ARTIFACT = (action) => {
     return new Promise((resolve, reject) => {
         const execString = `curl -u ${action.params.AUTH} -x PUT "${action.params.DEPLOY_TO}" -T ${action.params.FILE_PATH}`;
@@ -47,30 +46,8 @@ const DELETE_FROM_BINTRAY = (action) => {
 };
 
 
-const functions = {
+module.exports = {
     DEPLOY_AN_ARTIFACT,
     RETRIEVE_AN_ARTIFACT,
     DELETE_FROM_BINTRAY
 };
-
-function main(argv) {
-    if (argv.length < 3) {
-        console.log('{err: "not enough parameters"}');
-        // Invalid Argument
-        // Either an unknown option was specified, or an option requiring a value was provided without a value.
-        process.exit(9);
-    }
-    let action = JSON.parse(argv[2]);
-    functions[action.method.name](action)
-        .then(res => {
-            console.log(res);
-            process.exit(0); // Success
-        }).catch(err => {
-        console.log("an error occurred", err);
-        // Uncaught Fatal Exception
-        // There was an uncaught exception, and it was not handled by a domain or an 'uncaughtException' event handler.
-        process.exit(1);
-    });
-}
-
-main(process.argv);
